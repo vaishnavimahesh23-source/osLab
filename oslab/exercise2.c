ade1d8a7545c95e508bb9e37b61fc8ed36210d3c
vaishnavimahesh23-source <vaishnavimahesh23@gmail.com>
2026-07-16 08:08:58 +0000

    osalab programs

--- oslab/exercise2.c
+++ oslab/exercise2.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<fentl.h>
+#include<fcntl.h>
 #include<unistd.h>
 #include<sys/stat.h>
 #include<dirent.h>
@@ -12,14 +12,14 @@ int main()
     DIR*dir;
     struct dirent*entry;
     printf("\n---creating and writing file---\n");
-    fd=open("student.txt",O_CREAT|O_WRONGLY,0644);
+    fd=open("student.txt",O_CREAT|O_WRONLY,0644);
     if(fd<0)
     {
         printf("file creation failed\n");
         return 1;
     }
     char data[]="linux system calls experiment\n""B.Sc cyber security laboratory";
-    write(fd,data,srlen(data));
+    write(fd,data,strlen(data));
     close(fd);
     printf("data written successfully\n");
     printf("\n--reading file content---\n");
@@ -35,7 +35,32 @@ int main()
     close(fd);
     printf("\n---file information---\n");
     if(stat("student.txt",&fileinfo)==0){
-        printf("file sizze:ld bytes\n",fileinfo.st_size);
+        printf("file size:ld bytes\n",fileinfo.st_size);
         printf("number of links:%ld\n",fileinfo.st_nlink);
-        printf("permissions:%o\n")
+        printf("permissions:%o\n",fileinfo.st_mode&0777);
     }
+    else{
+        printf("unable to get file information\n");
+    }
+    printf("\n---creating directory---\n");
+    if(mkdir("testdirectory",0755)==0)
+    {
+        printf("directory created successfully\n");
+    }
+    else{
+        printf("directory may already exist\n");
+    }
+    printf("\n---directory contents---\n");
+    dir=opendir(".");
+    if(dir==NULL){
+        printf("cannot open directory\n");
+        return l;
+    }
+    while((entry=readdir(dir))!=NULL)
+    {
+        printf("%s\n",entry->d_name);
+    }
+    closedir(dir);
+    printf("\nprogram completed successfully\n");
+    return 0;
+}
